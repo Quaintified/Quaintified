0bee695dcfe83d73c62f54b536aa16051fabf18c
Quaint <thesiberiantigercat@gmail.com>
2022-03-11 12:59:57 -0500

    Fix NBT Command bug with not giving item.

--- src/main/java/com/dndcraft/quaint/quaintified/commands/custom/nbt/CommandNBT.java
+++ src/main/java/com/dndcraft/quaint/quaintified/commands/custom/nbt/CommandNBT.java
@@ -6,6 +6,7 @@ import com.dndcraft.atlas.util.AtlasColor;
 import com.dndcraft.atlas.util.ItemUtil;
 import com.dndcraft.quaint.quaintified.commands.BaseCommand;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -14,7 +15,7 @@ import java.util.Random;
 public class CommandNBT extends BaseCommand {
     private SubCommandCheck subCommandCheck = new SubCommandCheck();
 
-    public void invoke() {
+    public void invoke(Player player) {
         var name = AtlasPaper.get().componentBuilder().append("Your Present", AtlasColor.GRAY).build();
         var lore = AtlasPaper.get().componentBuilder().append("Merry Christmas!", AtlasColor.GREEN).build();
 
@@ -24,6 +25,8 @@ public class CommandNBT extends BaseCommand {
 
         ItemStack item = ItemUtil.make(Material.COAL, amount, name, lore);
         ItemUtil.setTag(item, "found-by", getSender().getName());
+
+        player.getInventory().addItem(item);
     }
 
     @Cmd(value = "check", permission = "quaintified.command.nbt.check")
